a251ecaea26bdbd63fe6aa97f9d912e99f208171
BuzyBeeC <brianc.gde@gmail.com>
2024-10-19 00:52:53 -0700

    Step 2: Implement marker drawing and clear in canvas

--- src/main.ts
+++ src/main.ts
@@ -19,4 +19,28 @@ if (!ctx) { throw new Error("Cannot get canvas context!"); }
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+const controls = document.createElement("div");
+app.appendChild(controls);
 
+const clearButton = document.createElement("button");
+clearButton.innerText = "Clear ✖";
+clearButton.addEventListener("click", () => {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+controls.appendChild(clearButton);
+
+ctx.strokeStyle = "#792de6"
+let isPainting = false;
+canvas.addEventListener("mousedown", (e) => {
+    isPainting = true;
+    ctx.moveTo(e.offsetX, e.offsetY);
+    ctx.beginPath();
+});
+canvas.addEventListener("mouseup", () => { isPainting = false; });
+canvas.addEventListener("mouseout", () => { isPainting = false; });
+canvas.addEventListener("mousemove", (e) => {
+    if (isPainting) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+});
