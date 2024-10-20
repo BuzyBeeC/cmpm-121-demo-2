014e8a8685cf15346746a50b74eb44ca6762719b
BuzyBeeC <brianc.gde@gmail.com>
2024-10-20 01:20:39 -0700

    Step 5: Refactor strokes to use command pattern

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,11 @@ canvas.width = 256;
 canvas.height = 256;
 const ctx = canvas.getContext("2d");
 if (!ctx) { throw new Error("Cannot get canvas context!"); }
-const clearCanvas = () => {
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
+
+function clearCanvas() {
+    ctx?.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 ctx.fillStyle = "white";
 clearCanvas();
 app.appendChild(canvas);
@@ -27,23 +29,46 @@ interface Point {
     x: number;
     y: number;
 }
-const strokes: Point[][] = [];
-const drawEvent = new Event("drawing-changed"); // Observer drawing command
+interface Stroke {
+    points: Point[];
+    display: (context: CanvasRenderingContext2D) => void;
+    drag: (x: number, y: number) => void;
+}
+const strokes: Stroke[] = [];
+function getLastStroke() {
+    return (strokes.length > 0) ? strokes[strokes.length - 1] : null;
+}
+// Stroke command pattern
+function createStroke(startX: number, startY: number): Stroke {
+    return {
+        points: [{
+            x: startX,
+            y: startY
+        }],
+        display: function (context) {
+            context.beginPath();
+            this.points.forEach(point => {
+                context.lineTo(point.x, point.y);
+                context.stroke();
+            });
+        },
+        drag: function (x, y) {
+            this.points.push({ x, y });
+        }
+    };
+}
+
+// Drawing observer pattern
+const drawEvent = new Event("drawing-changed");
 canvas.addEventListener("drawing-changed", () => {
     clearCanvas();
-    for (const stroke of strokes) {
-        ctx.beginPath();
-        for (const point of stroke) {
-            ctx.lineTo(point.x, point.y);
-            ctx.stroke();
-        }
-    }
+    strokes.forEach(stroke => stroke.display(ctx));
 });
 
 // Redo button
 const redoButton = document.createElement("button");
 redoButton.innerText = "Redo ↪";
-const redoStack: Point[][] = [];
+const redoStack: Stroke[] = [];
 redoButton.addEventListener("click", () => {
     const redoStroke = redoStack.pop();
     if (redoStroke) {
@@ -72,30 +97,27 @@ clearButton.addEventListener("click", () => {
     redoStack.length = 0;
 });
 
-// Drawing logic
-ctx.strokeStyle = "#792de6"
-let isPainting = false;
-canvas.addEventListener("mousedown", () => {
-    isPainting = true;
-    ctx.beginPath();
-    strokes.push([]);
-});
-canvas.addEventListener("mouseup", () => { isPainting = false; });
-canvas.addEventListener("mouseout", () => { isPainting = false; });
-canvas.addEventListener("mousemove", (e) => {
-    if (isPainting) {
-        strokes[strokes.length - 1].push({
-            x: e.offsetX,
-            y: e.offsetY
-        });
-        redoStack.length = 0;
-        canvas.dispatchEvent(drawEvent);
-    }
-});
-
 // Controls div
 const controls = document.createElement("div");
 app.appendChild(controls);
 controls.appendChild(undoButton);
 controls.appendChild(redoButton);
 controls.appendChild(clearButton);
+
+// Drawing logic
+ctx.strokeStyle = "#792de6";
+let isPainting = false;
+canvas.addEventListener("mousedown", (e) => {
+    isPainting = true;
+    strokes.push(createStroke(e.offsetX, e.offsetY));
+    redoStack.length = 0;
+    canvas.dispatchEvent(drawEvent);
+});
+canvas.addEventListener("mousemove", (e) => {
+    if (isPainting) {
+        getLastStroke()?.drag(e.offsetX, e.offsetY);
+        canvas.dispatchEvent(drawEvent);
+    }
+});
+canvas.addEventListener("mouseup", () => { isPainting = false; });
+canvas.addEventListener("mouseout", () => { isPainting = false; });
